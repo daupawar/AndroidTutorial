aef72e3571209e1cf37427988930af5cd0c9f50e
rohan <rohan.pawar@mobifilia.com>
2020-07-18 19:11:56 +0530

    login page added

---
+++ app/src/main/java/com/example/androidtutorial/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidtutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Snackbar.make(usernameEditText, "Please enter values", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
